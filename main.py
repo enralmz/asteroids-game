99e41aa10d39bffcde3d7e133333cde34fc98479
enralmz <enr.almazan@outlook.com>
2026-05-18 10:12:09 -0600

    added ship Movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame 
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,16 +9,25 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
 
         for event in pygame.event.get():
-            if event.type == event.QUIT:
+            if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000
 
 
 if __name__ == "__main__":
